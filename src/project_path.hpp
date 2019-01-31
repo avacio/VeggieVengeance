82ca45b2296c492bf68189222cf2efb0b8775705
Michael Zhang <mzhang@Michaels-MacBook-Pro-2.local>
2019-01-30 22:17:31 -0800

    Removing build folder

--- src/project_path.hpp
+++ src/project_path.hpp
@@ -2,4 +2,4 @@
 
 // Please don't change the content of this header
 
-#define PROJECT_SOURCE_DIR "C:/Users/jen_d/Desktop/CPSC 436D/team09game/"
+#define PROJECT_SOURCE_DIR "/Users/mzhang/Documents/CPSC436d/team09game/"
