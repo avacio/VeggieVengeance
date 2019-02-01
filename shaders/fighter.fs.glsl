8ea0eb862eafed25671ac835eecd89a2fa2457e4
Alexis Gervacio <k7c1b@ugrad.cs.ubc.ca>
2019-01-31 21:51:59 -0800

    track fighter shaders

---
+++ shaders/fighter.fs.glsl
@@ -0,0 +1,23 @@
+#version 330
+precision highp float;
+precision highp int;
+
+// From vertex shader
+in vec2 texcoord;
+
+// Application data
+uniform sampler2D sampler0;
+uniform bool is_hurt;
+uniform float time;
+
+//uniform vec3 fcolor;
+
+// Output color
+layout(location = 0) out  vec4 color;
+
+void main()
+{
+    if (is_hurt) {
+	    color = vec4(1.0, 0.7, 0.7, 1.0) * texture(sampler0, vec2(texcoord.x, texcoord.y));
+	} else { color = vec4(1.0, 1.0, 1.0, 1.0) * texture(sampler0, vec2(texcoord.x, texcoord.y)); }
+}
