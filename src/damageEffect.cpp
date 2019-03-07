8f9e5ab7d61e414db8e62f75b6273f8800179808
madigraf <grafmadison@gmail.com>
2019-03-06 22:39:59 -0800

    fixed variable names of damageEffect members

--- src/damageEffect.cpp
+++ src/damageEffect.cpp
@@ -1,8 +1,8 @@
 #include "damageEffect.hpp"
 
-DamageEffect::DamageEffect(int xpos, int ypos, int width, int height, unsigned int damage, int id, DeletionTime delete_when) : bounding_box(xpos, ypos, width, height) {
-	this->damage = damage;
-	this->id = id;
-	this->delete_when = delete_when;
-	hit_fighter = false;
+DamageEffect::DamageEffect(int xpos, int ypos, int width, int height, unsigned int damage, int id, DeletionTime delete_when) : m_bounding_box(xpos, ypos, width, height) {
+	this->m_damage = damage;
+	this->m_fighter_id = id;
+	this->m_delete_when = delete_when;
+	m_hit_fighter = false;
 }
--- src/damageEffect.hpp
+++ src/damageEffect.hpp
@@ -6,9 +6,9 @@ class DamageEffect
 {
 public:
 	DamageEffect(int xpos, int ypos, int width, int height, unsigned int damage, int id, DeletionTime delete_when);
-	BoundingBox bounding_box;
-	unsigned int damage; //the amount of damage this will deal
-	int id; //the fighter who used this move
-	DeletionTime delete_when;
-	bool hit_fighter; //whether or not a fighter has been hit by this move
+	BoundingBox m_bounding_box;
+	unsigned int m_damage; //the amount of damage this will deal
+	int m_fighter_id; //the fighter who used this move
+	DeletionTime m_delete_when;
+	bool m_hit_fighter; //whether or not a fighter has been hit by this move
 };
