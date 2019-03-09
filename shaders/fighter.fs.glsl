706b20c58214dba3ebeb8b6e4e00543c48a25580
Alireza Arbabian <g5w9a@ugrad.cs.ubc.ca>
2019-03-09 12:08:41 -0800

    Added blocking mechanic

--- shaders/fighter.fs.glsl
+++ shaders/fighter.fs.glsl
@@ -8,6 +8,7 @@ in vec2 texcoord;
 // Application data
 uniform sampler2D sampler0;
 uniform bool is_hurt;
+uniform bool is_blocking;
 uniform float time;
 
 //uniform vec3 fcolor;
@@ -19,5 +20,9 @@ void main()
 {
     if (is_hurt) {
 	    color = vec4(1.0, 0.7, 0.7, 1.0) * texture(sampler0, vec2(texcoord.x, texcoord.y));
-	} else { color = vec4(1.0, 1.0, 1.0, 1.0) * texture(sampler0, vec2(texcoord.x, texcoord.y)); }
+	}
+	if (is_blocking) {
+		color = vec4(0.5, 0.5, 1.0, 1.0) * texture(sampler0, vec2(texcoord.x, texcoord.y));
+	}
+	else { color = vec4(1.0, 1.0, 1.0, 1.0) * texture(sampler0, vec2(texcoord.x, texcoord.y)); }
 }
