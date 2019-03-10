a5da32d93adc37a9a1756e426805a12b250a8f73
Alireza Arbabian <g5w9a@ugrad.cs.ubc.ca>
2019-03-10 14:16:45 -0700

    fixed damage visual effect bug

--- shaders/fighter.fs.glsl
+++ shaders/fighter.fs.glsl
@@ -18,11 +18,14 @@ layout(location = 0) out  vec4 color;
 
 void main()
 {
-    if (is_hurt) {
-	    color = vec4(1.0, 0.7, 0.7, 1.0) * texture(sampler0, vec2(texcoord.x, texcoord.y));
-	}
 	if (is_blocking) {
 		color = vec4(0.5, 0.5, 1.0, 1.0) * texture(sampler0, vec2(texcoord.x, texcoord.y));
 	}
+
+    else if (is_hurt) {
+	    color = vec4(1.0, 0.7, 0.7, 1.0) * texture(sampler0, vec2(texcoord.x, texcoord.y));
+	}
+	
+
 	else { color = vec4(1.0, 1.0, 1.0, 1.0) * texture(sampler0, vec2(texcoord.x, texcoord.y)); }
 }
